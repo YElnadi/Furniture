9116a0b34acd4ca346d791bfbdb5ef6a2729ea56
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-10-07 14:35:33 -0700

    pagination

--- furniture/my_app/src/App.js
+++ furniture/my_app/src/App.js
@@ -2,32 +2,38 @@ import furnitures from "./furnitures";
 import FurnitureCard from "./Furniture/FurnitureCard";
 import { useState } from "react";
 
-const PAGE_SIZE = 5
+const PAGE_SIZE = 5;
 
-function App() {
-  const [pageIdx, setPageIdx] = useState(0)
+const App = () => {
+  const [pageIdx, setPageIdx] = useState(0);
   const furnitureItems = furnitures
-  .slice(PAGE_SIZE * pageIdx, PAGE_SIZE * (pageIdx + 1))
-  
-  .map((furniture, idx) => (
-    <FurnitureCard key={idx} furniture={furniture} />
-  ));
+    .slice(PAGE_SIZE * pageIdx, PAGE_SIZE * (pageIdx + 1))
+    .map((furniture, idx) => <FurnitureCard key={idx} furniture={furniture} />);
 
+  const numPages = Math.ceil(furnitures.length / PAGE_SIZE);
+
+  const pageButtons = [];
+  for (let i = 0; i < numPages; i += 1) {
+    pageButtons.push(<button key={i} onClick={()=> setPageIdx(i)} className="mx-1 bg-stone-300 w-4 text-stone-600 text-sm rounded-sm">{i + 1} </button>);
+  }
   return (
     <div className="flex flex-col items-center py-64 bg-stone-100 min-h-screen">
       <div className=" mx-4 mb-10 text-4xl text-stone-600 ">
         Autumn's Fabulous Furniture
       </div>
-      <div className="w-full max-w-2xl"> {furnitureItems}</div>
+      <div className="w-full max-w-2xl">
+        <div className="flex justify-end border-b border-stone-300 px-8 py-2 ">{pageButtons}</div>
+
+        {furnitureItems}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
 
-
 //page 0 : items 0 to 5 (not inclusive)
-//page 1 : items 5 to 10 
+//page 1 : items 5 to 10
 //page 2: items 10 to 15
 // page 3 : items 15 to 20
 
