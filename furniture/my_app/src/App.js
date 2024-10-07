2e630506b31575b4355df83450a77b927c4d2122
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-10-07 14:14:43 -0700

    pagination part 1

--- furniture/my_app/src/App.js
+++ furniture/my_app/src/App.js
@@ -1,13 +1,20 @@
 import furnitures from "./furnitures";
 import FurnitureCard from "./Furniture/FurnitureCard";
+import { useState } from "react";
+
+const PAGE_SIZE = 5
 
 function App() {
-  const furnitureItems = furnitures.map((furniture, idx) => (
+  const [pageIdx, setPageIdx] = useState(0)
+  const furnitureItems = furnitures
+  .slice(PAGE_SIZE * pageIdx, PAGE_SIZE * (pageIdx + 1))
+  
+  .map((furniture, idx) => (
     <FurnitureCard key={idx} furniture={furniture} />
   ));
 
   return (
-    <div className="flex flex-col items-center py-64 bg-stone-100 h-screen">
+    <div className="flex flex-col items-center py-64 bg-stone-100 min-h-screen">
       <div className=" mx-4 mb-10 text-4xl text-stone-600 ">
         Autumn's Fabulous Furniture
       </div>
@@ -17,3 +24,12 @@ function App() {
 }
 
 export default App;
+
+
+//page 0 : items 0 to 5 (not inclusive)
+//page 1 : items 5 to 10 
+//page 2: items 10 to 15
+// page 3 : items 15 to 20
+
+// formula to slice and display items correctly
+// page n : n * 5 to 5 * (n + 1)
